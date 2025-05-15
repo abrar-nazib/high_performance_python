7cff849e03bd86e0b32b5d48d7dec8ee52d9426e
abrar-nazib <abrarnazib@gmail.com>
2025-05-15 23:10:49 +0600

    Finish halfway through chapter 2 - Before visualization

---
+++ ch2/julia_set_wrapped.py
@@ -0,0 +1,67 @@
+"""Julia set generator"""
+
+import time
+
+
+def timefn(func):
+    def wrapper(*args, **kwargs):
+        t1 = time.time()
+        result = func(*args, **kwargs)
+        t2 = time.time()
+        print(f"@timefn: {func.__name__} took {t2 - t1:.4f} seconds")
+        return result
+
+    return wrapper
+
+
+x1, x2, y1, y2 = -1.8, 1.8, -1.8, 1.8
+c_real = -0.62882
+c_imag = -0.42193
+
+
+@timefn
+def calculate_z_serial_purepython(maxiter, zs, cs):
+    """Calculate output list using Julia update rule"""
+    output = [0] * len(zs)
+    for i in range(len(zs)):
+        n = 0
+        z = zs[i]
+        c = cs[i]
+        while abs(z) < 2 and n < maxiter:
+            z = z * z + c
+            n += 1
+        output[i] = n
+    return output
+
+
+def calc_pure_python(desired_width, max_iterations):
+    "Create a list of complex coordinates (zs) and complex parameters (cs), build Julia set"
+    x_step = (x2 - x1) / desired_width
+    y_step = (y1 - y2) / desired_width
+    x = []
+    y = []
+    ycoord = y2
+    while ycoord > y1:
+        y.append(ycoord)
+        ycoord += y_step
+    xcoord = x1
+    while xcoord < x2:
+        x.append(xcoord)
+        xcoord += x_step
+
+    zs = []
+    cs = []
+    for ycoord in y:
+        for xcoord in x:
+            zs.append(complex(xcoord, ycoord))
+            cs.append(complex(c_real, c_imag))
+
+    print("Length of x:", len(x))
+    print("Total elements:", len(zs))
+    output = calculate_z_serial_purepython(max_iterations, zs, cs)
+    assert sum(output)
+
+
+if __name__ == "__main__":
+    # Calculate the Julia set using pure python solution with reasonable defaults for a laptop
+    calc_pure_python(desired_width=1000, max_iterations=300)
